dc4f850f2a5de8a57003f15cbef2806df9087a17
Omkar Pathak <omkarpathak27@gmail.com>
2018-04-25 07:08:10 +0530

    Modified logo and some front-end changes

--- Gymnasium/settings.py
+++ Gymnasium/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k6_^w5l$q(85$r_vc*ou9q!aa8hh_aczk425&iw6w4qx$=cg=l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
